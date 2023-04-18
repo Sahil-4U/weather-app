9d60097b9b4d4571b49cf07608475e6f836eb04f
Sahil <ss6611786@gmail.com>
2023-04-18 23:19:05 +0530

    extract longnitude and latitude and their value with the help of navigator function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  const [log,setLog]=useState(0);
+  const [lat,setLat]=useState(0);
+  const [hemi,setHemi]=useState('');
+  const [month,setMonth]=useState('');
+
+  const handleClick=(e)=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let {latitude,longitude}=position.coords;
+      console.log(longitude+"  "+latitude);
+      setLog(longitude);
+      setLat(latitude);
+    })
+    //get the hemisphere
+    if(lat>0){
+      setHemi("Northern Hemisphere");
+    }else if(lat<0){
+      setHemi("Southern Hemisphere");
+    }else{
+      setHemi("Equator");
+    }
+    //get the month
+    const date=new Date();
+    let currentMonth=date.getMonth();
+    setMonth(currentMonth+1);
+  }
+  return(
+
+    <div>
+      <center>
+      <h2>Lognitude:{log} <br/>  Latitude:{lat}</h2>
+      <h3>Hemisphere:{hemi}</h3>
+      <h3>Month:{month}</h3>
+      <button onClick={handleClick}>Get my location</button>
+      </center>
     </div>
-  );
+  )
 }
 
 export default App;
